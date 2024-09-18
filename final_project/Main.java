23ee43610623f56ed3b9a88dfb889bdfcce86a0c
Hyorii88 <22014000@st.phenikaa-uni.edu.vn>
2024-09-18 10:29:38 +0700

    Nhap sinh vien chua co lien ket du lieu tu sql

--- final_project/Main.java
+++ final_project/Main.java
@@ -9,6 +9,14 @@ class Sinhvien
     private int Diem;
     private int Kyhoc;
 
+    public Sinhvien (int ID, String Name, int Diem, String Nganh, int Kyhoc)
+    {
+        this.ID=ID;
+        this.Name=Name;
+        this.Nganh=Nganh;
+        this.Diem=Diem;
+        this.Kyhoc=Kyhoc;
+    }
     /*Phuong thuc get */
     public int getID()
     {
@@ -64,10 +72,12 @@ class Sinhvien
 class danhsachsinhvien
 {
     private ArrayList<Sinhvien> danhsach;
-    
+
     public danhsachsinhvien()
     {
+        danhsach=new ArrayList<>();
     }
+    
     public danhsachsinhvien(ArrayList<Sinhvien> danhsach)
     {
         this.danhsach=danhsach;
@@ -77,6 +87,14 @@ class danhsachsinhvien
     {
         this.danhsach.add(sv);
     }
+
+    public void insinhvien()
+    {
+        for (Sinhvien sv : danhsach)
+        {
+            System.out.println(" ID= "+ sv.getID() + "Name=" + sv.getName());
+        }
+    }
 }
 
 public class Main
@@ -85,18 +103,21 @@ public class Main
     {
         Scanner sc = new Scanner(System.in);
         danhsachsinhvien dssv = new danhsachsinhvien(); 
-        for(int i=0;i<10;i++)
+        for(int i=0;i<1;i++)
         {   
             System.out.println("Nhap thong tin sinh vien thu" + (i+1));
             if (i>=1){                                    
             String b=sc.nextLine();}
-            System.out.println("Nhap ma sinh vien:"); String ID =sc.nextLine();
+            System.out.println("Nhap ma sinh vien:"); int ID=sc.nextInt();
+                                                 String b=sc.nextLine();
             System.out.println("Nhap ten:"); String Name =sc.nextLine();                                 
             System.out.println("Nhap Diem:"); int Diem =sc.nextInt();
                                                 String a=sc.nextLine();
             System.out.println("Nhap Nganh:"); String Nganh =sc.nextLine();
-            System.out.println("Nhap Ky hoc:"); int Kyhoc =sc.nextInt();
-
+             System.out.println("Nhap Ky hoc:"); int Kyhoc =sc.nextInt();
+            Sinhvien sv =new Sinhvien(ID, Name, Diem,Nganh, Kyhoc);
+            dssv.themsinhvien(sv);
+            dssv.insinhvien();
         }
     }
 }
---
+++ final_project/Monhoc.java
@@ -0,0 +1,11 @@
+class Monhoc {   
+private String tenMonhoc;
+private String soTin;
+private String nguoiDay;
+private String Lop;
+}
+
+class danhsachMonhoc
+{
+    
+}
